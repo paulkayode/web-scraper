81a5e299d43e662f5a9a23dc973dde7f0e1575fd
segunkayode1 <segunkayode456@gmail.com>
2024-01-13 01:07:42 +1100

    Add normaliseURL function and test

--- crawl.js
+++ crawl.js
@@ -1,6 +1,11 @@
 
-function normaliseURL() {
-
+function normaliseURL(url) {
+    let i = 0;
+    while(url[i] !== '/')i++;
+    while(url[i] === '/')i++;
+    let j = i;
+    while(url[j] !== '/')j++;
+    return url.slice(i,j);
 }
 
 module.exports = {
---
+++ crawl.test.js
@@ -0,0 +1,9 @@
+const crawl = require('./crawl');
+
+test('Testing Normalise Url',()=>{
+     const testUrls = ['https://blog.boot.dev/path/', 'https://blog.boot.dev/path', 'http://blog.boot.dev/path/','http://blog.boot.dev/path']
+     const expected = 'blog.boot.dev'
+     for(let testUrl of testUrls){
+        expect(crawl.normaliseURL(testUrl)).toBe(expected);
+     }
+});
