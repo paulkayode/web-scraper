d6383662124c1de7359fc8af53bf935c9be38ceb
segunkayode1 <segunkayode456@gmail.com>
2024-01-13 10:21:16 +1100

    Fix URL normalization logic

--- crawl.js
+++ crawl.js
@@ -1,10 +1,11 @@
 
 function normaliseURL(url) {
     let i = 0;
-    while(url[i] !== '/')i++;
-    while(url[i] === '/')i++;
+    while(url[i] !== '/' && i < url.length)i++;
+    if(i >= url.length || (i +1 < url.length && url[i+1] !== '/')) i = 0;
+    while(url[i] === '/' && i < url.length)i++;
     let j = i;
-    while(url[j] !== '/')j++;
+    while(url[j] !== '/' &&  j < url.length)j++;
     return url.slice(i,j);
 }
 
--- crawl.test.js
+++ crawl.test.js
@@ -1,7 +1,7 @@
 const crawl = require('./crawl');
 
 test('Testing Normalise Url',()=>{
-     const testUrls = ['https://blog.boot.dev/path/', 'https://blog.boot.dev/path', 'http://blog.boot.dev/path/','http://blog.boot.dev/path']
+     const testUrls = ['https://blog.boot.dev/path/', 'https://blog.boot.dev/path', 'http://blog.boot.dev/path/','http://blog.boot.dev/path','blog.boot.dev','blog.boot.dev/path']
      const expected = 'blog.boot.dev'
      for(let testUrl of testUrls){
         expect(crawl.normaliseURL(testUrl)).toBe(expected);
