74e015470c011cf82157a9023fe22ba715f70621
segunkayode1 <segunkayode456@gmail.com>
2024-01-13 11:38:26 +1100

    Add command line argument validation and start web crawler at specified URL

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+const { argv } = require('node:process');
+
+
+function main(){
+     if(argv.length !== 3){
+        console.log(`Incorrect amount of arguments ${argv.length -2} arguments provided, only one Argument is Needed`);
+        return 0;
+     }
+     let baseUrl = argv[2];
+     console.log(`web-crawler starting at ${baseUrl}`);
+}
+
+main()
